691f96c9981a78340ff9887f821ea3e6403f0258
Sam Hinshaw <samuel.hinshaw@gmail.com>
2017-09-20 00:53:55 -0700

    Python logic for calculating points!
    NOTE: Save running total (not day by day) for server, not python, as we want to be able to exclude any future dates or let the user specify ranges!
    - also attempting to show points in view… not working?

--- data/get_single_day.py
+++ data/get_single_day.py
@@ -1,6 +1,10 @@
 """
 Python script for pulling user's data from MyFitnessPal
 """
+# since we're using python 2, we need to import this. OTHERWISE when calculating
+# points for exercise we get stuck with 0s because int/int = int. So 14/15 = 0.
+# SUCKS!!! I should switch to python 3.
+from __future__ import division
 import sys
 import re  # regex library
 # import json
@@ -27,7 +31,6 @@ user = sys.argv[2]
 # now = arrow.utcnow()
 date = arrow.get(inputDate, 'YYYY-MM-DD', tzinfo='US/Pacific')
 now = arrow.now('US/Pacific')
-print now
 # dateObj = {'year': date.year, 'month': date.month, 'day': date.day}
 
 ############
@@ -63,62 +66,6 @@ else:
 
 print 'Pulling in MyFitnessPal information for ' + user + '...'
 
-# Try/Except is mostly for if the API fails
-try:
-    MFPcals = MFPclient.get_date(date.year, date.month, date.day)
-
-    # Check to see if the totals object is empty. If so, the entry is most
-    # likely empty, and we can just return zero values for everything
-    if 'calories' in MFPcals.totals:
-        totalCals = MFPcals.totals['calories']
-        goalCals = MFPcals.goals['calories']
-        netCals = goalCals - totalCals
-        isEmpty = False
-    else:
-        totalCals = 0
-        goalCals = 0
-        netCals = 0
-        isEmpty = True
-
-except:
-    sys.exit('There was an error retrieving your data from MyFitnessPal.')
-
-# Workout points
-
-# cardioExercises = ['jogging', 'dancing', 'SUP']
-# lowIntensityExercises = ['yoga', 'stretching']
-# crossTrainingExercises = ['XT']
-
-#   if (str_detect(name, 'jog'))      { return('cardio') }
-#   if (str_detect(name, 'yoga'))     { return('yoga') }
-#   if (str_detect(name, 'train'))    { return('XT') }
-#   if (str_detect(name, 'danc'))     { return('dancing') }
-#   if (str_detect(name, 'stretch'))  { return('stretching') }
-
-# A specific error is better than a silent error
-try:
-    MFPexer = MFPclient.get_exercise(date.year, date.month, date.day)
-except:
-    sys.exit('We were not able to retrieve your exercise for this date!')
-exercises = []
-if len(MFPexer[0].entries) > 0:
-    for n in range(0, len(MFPexer[0].entries)):
-        # if MFPexer[0].entries[n].name in lowIntensityExercises:
-        #     points = MFPexer[0].entries[n]['minutes'] / 60
-        # elif MFPexer[0].entries[n].name in cardioExercises:
-        #     points = MFPexer[0].entries[n]['minutes'] / 30
-        # elif MFPexer[0].entries[n].name in crossTrainingExercises:
-        #     points = MFPexer[0].entries[n]['minutes'] / 15
-        # else:
-        #     points = 0
-
-        exercises.append({
-            'name': MFPexer[0].entries[n].name,
-            'minutes': MFPexer[0].entries[n]['minutes'],
-            'cals': MFPexer[0].entries[n]['calories burned']
-            # 'points': points
-        })
-
 # Calculate points based on that day's statistics!
 # Current values are:
 # NET CALS:
@@ -131,47 +78,111 @@ if len(MFPexer[0].entries) > 0:
 #   - cardio = 1pt/30min
 #   - XT     = 1pt/15min
 
+# Try/Except is mostly for if the API fails
+try:
+    MFPcals = MFPclient.get_date(date.year, date.month, date.day)
+
+    # Check to see if the totals object is empty. If so, the entry is most
+    # likely empty, and we can just return zero values for everything
+    if 'calories' in MFPcals.totals:
+        totalCals = MFPcals.totals['calories']
+        goalCals = MFPcals.goals['calories']
+        netCals = goalCals - totalCals
+        isEmpty = False
+
+        if netCals >= 0:
+            calPoints = 1
+        elif netCals < -300:
+            calPoints = -1
+        else:
+            calPoints = 0
+    else:
+        totalCals = 0
+        goalCals = 0
+        netCals = 0
+        isEmpty = True
+        calPoints = -1
+
+except:
+    sys.exit('There was an error retrieving your data from MyFitnessPal.')
+
 # First we need to sanitize our exercise names. We can't rely on good inputs
 # because of things like connected apps auto-inserting exercises.
 
-# calculateExPoints <- function(name, minutes) {
-#   if (is.null(name)) {
-#     return(0)
-#   } else if (is.na(name)) {
-#     return(0)
-#   } else if (minutes == 0) {
-#     return(0)
-#   } else if (name %in% c('jogging', 'dancing', 'SUP')) {
-#     return(minutes / 30)
-#   } else if (name == 'XT') {
-#     return(minutes / 15)
-#   } else if (name %in% c('yoga', 'stretching')) {
-#     return(minutes / 60)
-#   } else {
-#     return(0)
-#   }
-# }
+# Workout points
 
-# calculateCalPoints <- function(calsRem) {
-#   # -1pt if not filled out
-#   if (is.null(calsRem)) {
-#     return(-1)
-#   # -1pt if not filled out
-#   } else if (is.na(calsRem)) {
-#     return(-1)
-#   # 1pt for ending with cals left or at 0
-#   } else if (calsRem >= 0) {
-#     return(1)
-#   # No points if within 300 cals of target
-#   } else if (calsRem < 0 & calsRem > -300) {
-#     return(0)
-#   # -1pt if over 300 over that day
-#   }  else if (calsRem < -300) {
-#     return(-1)
-#   } else {
-#     return(0)
-#   }
-# }
+cardioExercises = ['cardio', 'dancing', 'SUP']
+lowIntensityExercises = ['yoga', 'stretching']
+crossTrainingExercises = ['XT']
+
+# Create dictionary for replacement
+exerciseDictionary = {
+    'jog': 'cardio',
+    'yoga': 'yoga',
+    'train': 'XT',
+    'danc': 'dancing',
+    'stretch': 'stretching'
+}
+
+# A specific error is better than a silent error
+try:
+    MFPexer = MFPclient.get_exercise(date.year, date.month, date.day)
+    exerEntries = MFPexer[0].entries
+
+except:
+    sys.exit('We were not able to retrieve your exercise for this date!')
+
+# set up empty exercise array to handle multiple exercises in a day
+exercises = []
+totExerPoints = 0
+
+# loop through all of the exercises in a day (if more than 0)
+if len(exerEntries) > 0:
+    for n in range(0, len(exerEntries)):
+
+        # rename complex variables
+        exerName = exerEntries[n].name
+        exerMins = exerEntries[n]['minutes']
+        exerCals = exerEntries[n]['calories burned']
+
+        # check for exercises and set name to key
+        for key in exerciseDictionary:
+            # .group(0) to extract result from match object
+            exerNameMatch = re.search(key, exerName)
+            # if match was found, pull out answer!
+            if exerNameMatch is not None:
+                matchedEx = exerNameMatch.group(0)
+                # then change from key to value
+                renamedEx = exerciseDictionary[matchedEx]
+            # there may be sme cases in the future in which there are multiple
+            # matches. In this case, we would simply expect to get the LAST
+            # result back... I do not anticipate multiple matches, but will
+            # adjust down the road if necessart
+
+            # award points based on workout type
+        if renamedEx in lowIntensityExercises:
+            points = (exerMins / 60)
+        elif renamedEx in cardioExercises:
+            points = (exerMins / 30)
+        elif renamedEx in crossTrainingExercises:
+            points = (exerMins / 15)
+        else:
+            points = 0
+
+        # Then round to 2 decimal places
+        points = round(points, 1)
+
+        totExerPoints = totExerPoints + points
+
+        exercises.append({
+            'name': renamedEx,
+            'minutes': exerMins,
+            'cals': exerCals,
+            'points': points
+        })
+
+# Final tally of points, add exercise points + calorie points
+totalDaysPoints = calPoints + totExerPoints
 
 # construct object for db insertion
 MFPdata = {
@@ -181,6 +192,7 @@ MFPdata = {
     'netCals': netCals,
     'exercise': exercises,
     'isEmpty': isEmpty,
+    'points': totalDaysPoints
     # 'lastUpdated': now.datetime
 }
 
@@ -201,32 +213,3 @@ if collection.find_one({'date': date.datetime}):
 else:
     print 'No data found yet for this date, creating record...'
     collection.insert_one(MFPdata)
-
-# define how we get calorie summary
-# def mapDateCals(year, month, day):
-#     try:
-#         date = samMFP.get_date(year, month, day)
-#         calsOnDate = MFPcals.totals['calories']
-#         return calsOnDate
-#     except:
-#         return None
-
-# define how we get exercise summary
-# def mapDateEx(year, month, day):
-#     try:
-#         MFPexer = samMFP.get_exercise(year, month, day)
-#         catFilename = 'temp/exercise/' + year + '_' + month + '_' + day + '_sam_exercise.json'
-#         df = []
-#         if len(MFPexer[0].entries) > 0:
-#             for n in range(0, len(MFPexer[0].entries)):
-#                 df.append({
-#                     'name': MFPexer[0].entries[n].name,
-#                     'minutes': MFPexer[0].entries[n]['minutes'],
-#                     'cals': MFPexer[0].entries[n]['calories burned']
-#                 })
-#             with open(catFilename, 'w') as outfile:
-#                 json.dump(df, outfile)
-#         else:
-#             return None
-#     except:
-#         return None
