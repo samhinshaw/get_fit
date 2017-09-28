c059738141b63440391852c31ecaebbf89c5cdaf
Sam Hinshaw <samuel.hinshaw@gmail.com>
2017-09-27 22:55:33 -0700

    Begin using Grunt
    - Move Bulma from Bower to NPM management
    - Use basic Grunt scripts for deployment
    - This is FANTASTIC for me as a beginner, I get to start with the easy stuff first before moving to web pack
    - This way I can easily conceptualize how all the pieces are moving because I’m writing the scripts myself
    - I did this to enable us to compile the Bulma Sass to CSS to give us greater flexibility when customizing our CSS.
    - Now I just need to take a crash course in Sass (after I finish this current course on node + express)

---
+++ Gruntfile.js
@@ -0,0 +1,66 @@
+/*global module:false*/
+module.exports = function(grunt) {
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    // banner:
+    //   '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+    //   '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+    //   '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+    //   '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+    //   ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+    // Task configuration.
+    sass: {
+      dist: {
+        files: {
+          'public/css/bulma.css': 'node_modules/bulma/bulma.sass'
+        }
+      }
+    },
+    watch: {
+      css: {
+        files: '**/*.scss',
+        tasks: ['sass']
+      }
+    }
+    // Use this Concat //
+    // concat: {
+    //   options: { separator: ';' },
+    //   build: {
+    //     src: ['js/file1.js', 'js/file2.js'],
+    //     dest: 'js/app.js'
+    //   }
+    // }
+    // concat: {
+    //   options: {
+    //     banner: '<%= banner %>',
+    //     stripBanners: true
+    //   },
+    //   dist: {
+    //     src: ['lib/<%= pkg.name %>.js'],
+    //     dest: 'dist/<%= pkg.name %>.js'
+    //   }
+    // }
+    // uglify: {
+    //   options: {
+    //     banner: '<%= banner %>'
+    //   },
+    //   dist: {
+    //     src: '<%= concat.dist.dest %>',
+    //     dest: 'dist/<%= pkg.name %>.min.js'
+    //   }
+    // }
+  });
+
+  // These plugins provide necessary tasks.
+  // grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  // grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task.
+  grunt.registerTask('default', ['watch']);
+  grunt.registerTask('dev', ['sass']);
+  // grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
+};
