da86647ae537099766b4a667708f91437f329e16
Sam Hinshaw <samuel.hinshaw@gmail.com>
2018-01-05 15:41:12 -0800

    Use moment JS in D3 script

--- public/js/data.js
+++ public/js/data.js
@@ -1,4 +1,5 @@
 /* global d3 */
+/* global moment */
 
 // async function asyncQuery() {
 //   const response = await $.getJSON('/api/user_weight/', json => json);
@@ -17,49 +18,79 @@
 //   { name: 'Emily', math: 80, science: 94 }
 // ];
 
+// Initialize Moment-Range
+window['moment-range'].extendMoment(moment);
+
+// Set up dates
+const now = moment.tz('US/Pacific');
+const today = now.clone().startOf('day');
+// const twoWeeksAgo = today.clone().subtract(14, 'days');
+const twoWeeksFromNow = today.clone().add(14, 'days');
+const oneMonthAgo = today.clone().subtract(1, 'months');
+
+// Enable custom domain?
+// const dateRange = [oneMonthAgo.toDate(), twoWeeksFromNow.toDate()];
+// const momentRange = moment.range(oneMonthAgo, twoWeeksFromNow);
+// const dateRange = [new Date(2017, 10, 1), new Date(2018, 6, 30)];
+const dateRange = false;
+
 // Set D3 locale
 
-const locale = d3.timeFormatLocale({
-  dateTime: '%x, %X',
-  date: '%-m/%-d/%Y',
-  time: '%-I:%M:%S %p',
-  periods: ['AM', 'PM'],
-  days: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
-  shortDays: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-  months: [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December'
-  ],
-  shortMonths: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-});
+// const locale = d3.timeFormatLocale({
+//   dateTime: '%x, %X',
+//   date: '%-m/%-d/%Y',
+//   time: '%-I:%M:%S %p',
+//   periods: ['AM', 'PM'],
+//   days: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+//   shortDays: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+//   months: [
+//     'January',
+//     'February',
+//     'March',
+//     'April',
+//     'May',
+//     'June',
+//     'July',
+//     'August',
+//     'September',
+//     'October',
+//     'November',
+//     'December'
+//   ],
+//   shortMonths: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+// });
 
 $.getJSON('/api/user_weight.json/', json => {
   // Pre-parse date
   const data = [];
-  const parseDate = locale.parse('%B %d, %Y at %I:%M%p%Z');
-  // const formatDate = locale.format('%s');
+  // const parseDate = locale.parse('%B %d, %Y at %I:%M%p%Z');
   // example: June 07, 2017 at 09:01AM
-  // dateFormat('June 07, 2017 at 09:01AM-0700');
+  // parseDate('June 07, 2017 at 09:01AM-0700');
 
   // const map = d3.map(json.rows, d => formatDate(parseDate(`${d.date}-0700`)));
   // console.log(map);
 
-  json.rows.forEach(d => {
-    data.push({
-      date: parseDate(`${d.date}-0700`),
-      weight: +d.weight
+  if (dateRange) {
+    // if we're using a custom domain...
+    json.rows.forEach(d => {
+      // only add to the array if the the date falls within our custom domain
+      if (moment.tz(d.date, 'MMMM DD, YYYY [at] hh:mmA', 'US/Pacific').within(momentRange)) {
+        data.push({
+          date: moment.tz(d.date, 'MMMM DD, YYYY [at] hh:mmA', 'US/Pacific').toDate(),
+          weight: +d.weight
+        });
+      }
     });
-  });
+    // if we're not using a custom domain, just push all the dates
+  } else {
+    json.rows.forEach(d => {
+      data.push({
+        date: moment.tz(d.date, 'MMMM DD, YYYY [at] hh:mmA', 'US/Pacific').toDate(),
+        weight: +d.weight
+      });
+    });
+  }
+
   // console.log(data);
 
   // const nestedData = d3
@@ -71,15 +102,8 @@ $.getJSON('/api/user_weight.json/', json => {
   const width = 960 - margin.left - margin.right;
   const height = 500 - margin.top - margin.bottom;
 
-  const x = d3
-    .scaleTime()
-    // .domain([new Date(2016, 11, 1), new Date(2018, 6, 30)])
-    .range([0, width]);
-
-  const y = d3
-    .scaleLinear()
-    // .domain([150, 190])
-    .range([height, 0]);
+  const x = d3.scaleTime().range([0, width]);
+  const y = d3.scaleLinear().range([height, 0]);
 
   const xAxis = d3.axisBottom().scale(x);
   const yAxis = d3.axisLeft().scale(y);
@@ -100,9 +124,16 @@ $.getJSON('/api/user_weight.json/', json => {
       .x(d => x(d.date))
       .y(d => y(d.weight));
 
-    x.domain(d3.extent(data, d => d.date));
-    y.domain(d3.extent(data, d => d.weight));
-    // y.domain([150, 190]);
+    if (dateRange) {
+      // Custom Domains
+      x.domain(dateRange).clamp(true);
+      y.domain([150, 190]);
+    } else {
+      // Automatic Domains
+      x.domain(d3.extent(data, d => d.date));
+      // y.domain(d3.extent(data, d => d.weight));
+      y.domain([d3.min(data, d => d.weight) - 1, d3.max(data, d => d.weight) + 1]);
+    }
 
     svg
       .append('g')
@@ -113,7 +144,7 @@ $.getJSON('/api/user_weight.json/', json => {
       .style('font-size', '12px')
       .attr('class', 'x axis')
       .attr('transform', `translate(0,${height})`)
-      .call(xAxis.tickFormat(d3.timeFormat('%b %Y')))
+      .call(xAxis.tickFormat(d3.timeFormat('%d %b %Y')))
       .selectAll('text')
       .style('text-anchor', 'end')
       .attr('dx', '-.8em')
