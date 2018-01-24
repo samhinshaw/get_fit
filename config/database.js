02be702fe9d8b43bda0e7cff32a129b32920ec9c
Sam Hinshaw <samuel.hinshaw@gmail.com>
2018-01-24 14:35:55 -0800

    Setting up babel transpile for production

--- config/database.js
+++
@@ -1,5 +0,0 @@
-export default {
-  database: 'mongodb://127.0.0.1:27017/get_fit',
-  serverPort: 8005,
-  startDate: '11-06-2017'
-};
--- getfit.config.js
+++ getfit.config.js
@@ -15,30 +15,32 @@ module.exports = {
         NODE_ENV: 'production'
       }
     }
-  ]
+  ],
 
   // Deployment section
   // http://pm2.keymetrics.io/docs/usage/deployment/
 
-  // deploy: {
-  //   production: {
-  //     user: 'node',
-  //     host: '212.83.163.1',
-  //     ref: 'origin/master',
-  //     repo: 'git@github.com:repo.git',
-  //     path: '/var/www/production',
-  //     'post-deploy': 'npm install && pm2 reload getfit.config.js --env production'
-  //   },
-  //   dev: {
-  //     user: 'node',
-  //     host: '212.83.163.1',
-  //     ref: 'origin/master',
-  //     repo: 'git@github.com:repo.git',
-  //     path: '/var/www/development',
-  //     'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env dev',
-  //     env: {
-  //       NODE_ENV: 'dev'
-  //     }
-  //   }
-  // }
+  deploy: {
+    production: {
+      user: 'sam',
+      host: 'samhinshaw.com',
+      ref: 'origin/master',
+      repo: 'git@github.com:samhinshaw/get_fit.git',
+      path: '/home/sam/serve/get_fit',
+      ssh_options: 'ForwardAgent=yes',
+      'post-deploy':
+        'yarn install && yarn build && pm2 startOrRestart getfit.config.js --env production'
+    }
+    // dev: {
+    //   user: 'node',
+    //   host: '212.83.163.1',
+    //   ref: 'origin/master',
+    //   repo: 'git@github.com:repo.git',
+    //   path: '/var/www/development',
+    //   'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env dev',
+    //   env: {
+    //     NODE_ENV: 'dev'
+    //   }
+    // }
+  }
 };
