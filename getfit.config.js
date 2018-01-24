305ab19407ad49c631c39667d5934f980f316464
Sam Hinshaw <samuel.hinshaw@gmail.com>
2018-01-24 15:10:31 -0800

    Make clean and build separate commands

--- getfit.config.js
+++ getfit.config.js
@@ -29,7 +29,7 @@ module.exports = {
       path: '/home/sam/serve/get_fit',
       ssh_options: 'ForwardAgent=yes',
       'post-deploy':
-        'ln -sf ~/secret/get_fit/* ./config/secret && yarn install && yarn build && pm2 startOrRestart getfit.config.js --env production',
+        'ln -sf ~/secret/get_fit/* ./config/secret && yarn install && yarn clean && yarn build && pm2 startOrRestart getfit.config.js --env production',
       env: {
         PATH:
           '/home/sam/.yarn/bin:/home/sam/.npm-global/bin:/home/sam/.miniconda3/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/home/sam/.local/bin',
