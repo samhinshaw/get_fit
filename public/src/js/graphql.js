9da6976e8e1ba9ff4797b61a0d644c354f7be3da
Sam Hinshaw <samuel.hinshaw@gmail.com>
2018-01-27 19:37:40 -0800

    Continuing graphql tutorial

--- public/src/js/graphql.js
+++ public/src/js/graphql.js
@@ -5,36 +5,34 @@ const client = new Lokka({
   transport: new Transport(`${window.location.origin}/graphql`)
 });
 
-const hiQuery = `
-  {
-    hi
-  }
-`;
-
 const queryEntries = `
 {
-  entries {
-    _id
-    netCals
+  users {
+    firstname
   }
 }
+
 `;
 
 // Hello
 
-client.query(hiQuery).then(result => {
-  // Log the result
-  console.log(result);
-  // Then change the HTML
-  // $('#userTitle').html(result.hi);
-  $('#userTitle').animate({ opacity: 0 }, 200, () => {
-    $('#userTitle')
-      .html(result.hi)
-      .animate({ opacity: 1 }, 200);
-  });
-});
+// client.query(hiQuery).then(result => {
+//   // Log the result
+//   console.log(result);
+//   // Then change the HTML
+//   // $('#userTitle').html(result.hi);
+//   $('#userTitle').animate({ opacity: 0 }, 200, () => {
+//     $('#userTitle')
+//       .html(result.hi)
+//       .animate({ opacity: 1 }, 200);
+//   });
+// });
 
 client.query(queryEntries).then(result => {
-  // Log the result
   console.log(result);
+  // Log the result
+  // const user = result.users[0].firstname;
+  // const partner = result.users[1].firstname;
+  // $('#userName').html(user.charAt(0).toUpperCase() + user.slice(1));
+  // $('#partnerName').html(partner.charAt(0).toUpperCase() + partner.slice(1));
 });
