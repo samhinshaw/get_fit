24104ce2a1bf35b249b3dfcb8113fa937320e4f1
Sam Hinshaw <samuel.hinshaw@gmail.com>
2018-11-11 18:34:18 -0800

    Initialize db with auth users

--- mongo/init.js
+++ mongo/init.js
@@ -1,5 +1,20 @@
 require('dotenv').config();
 
+// Make user for python script
+db.createUser({
+  user: process.env.MONGO_GETFIT_PYTHON_USER,
+  pwd: process.env.MONGO_GETFIT_PYTHON_PASS,
+  roles: [{ role: 'readWrite', db: 'get_fit' }]
+});
+
+// Make user for node connection
+db.createUser({
+  user: process.env.MONGO_GETFIT_NODE_USER,
+  pwd: process.env.MONGO_GETFIT_NODE_PASS,
+  roles: [{ role: 'readWrite', db: 'get_fit' }]
+});
+
+// Insert a dummy user
 db.users.insert({
   username: 'jon',
   password: '$2y$10$WJf9in/vfPMJjX3djGfxqOlf1F3XNbKeRXfFMymYnKzoUA6PMof.y',
