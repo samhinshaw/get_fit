817e16c84d881e8a272a9f87d39ff34a0042432c
Sam Hinshaw <samuel.hinshaw@gmail.com>
2018-12-31 07:56:52 -0800

    Replace newlines in necessary .env file params

--- src/app.js
+++ src/app.js
@@ -49,13 +49,14 @@ const productionEnv = process.env.NODE_ENV === 'production';
 // Bring in remaining config files
 const appConfig = require('../config/app_config.json');
 
-// Set up google credentials with secret parameters
-
+// Set up google credentials with secret parameters. Docker doesn't parse
+// start/end quotes within .env files, so make sure those are not present
 const googleCreds = {
   type: 'service_account',
   project_id: process.env.GOOGLE_PROJECT_ID,
   private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
-  private_key: process.env.GOOGLE_PRIVATE_KEY,
+  // docker doesn't parse newlines in .env files, so we need to replace manually
+  private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
   client_email: process.env.GOOGLE_CLIENT_EMAIL,
   client_id: process.env.GOOGLE_CLIENT_ID,
   auth_uri: 'https://accounts.google.com/o/oauth2/auth',
