d86a5f9c8a82c6cc5b50c316f85c122bde196d9f
Sam Hinshaw <samuel.hinshaw@gmail.com>
2019-01-13 14:59:29 -0800

    Run request in each test

--- tests/app.spec.js
+++ tests/app.spec.js
@@ -5,17 +5,17 @@ const Request = require('request');
 
 describe('Server', () => {
   describe('GET /', () => {
-    const data = {};
-    beforeAll(done => {
-      Request.get('http://node:8005/', (error, response, body) => {
+    it('Status 200', done => {
+      Request.get('http://node:8005/', (error, response) => {
         if (error) console.error(error); // eslint-disable-line no-console
-        data.status = response.statusCode;
-        data.body = body;
-        done();
+        if (response) {
+          expect(response.statusCode).toBe(200);
+          done();
+        } else {
+          fail();
+          done();
+        }
       });
-    });
-    it('Status 200', () => {
-      expect(data.status).toBe(200);
     });
   });
 });
