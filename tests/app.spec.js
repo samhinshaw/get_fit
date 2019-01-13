5d40fdbff285298303073925f295d858c60322e1
Sam Hinshaw <samuel.hinshaw@gmail.com>
2019-01-13 14:43:12 -0800

    Print backend test errors

--- tests/app.spec.js
+++ tests/app.spec.js
@@ -4,19 +4,11 @@ const Request = require('request');
 // const server = require('../src/app');
 
 describe('Server', () => {
-  // let server;
-  beforeAll(() => {
-    // use this if we're not
-    // eslint-disable-next-line global-require
-    // server = require('./app');
-  });
-  afterAll(() => {
-    // server.close();
-  });
   describe('GET /', () => {
     const data = {};
     beforeAll(done => {
       Request.get('http://node:8005/', (error, response, body) => {
+        if (error) console.error(error); // eslint-disable-line no-console
         data.status = response.statusCode;
         data.body = body;
         done();
@@ -25,24 +17,5 @@ describe('Server', () => {
     it('Status 200', () => {
       expect(data.status).toBe(200);
     });
-    // it('Body', () => {
-    //   expect(data.body).toBe('The Polyglot Developer');
-    // });
   });
-  // describe('GET /test', () => {
-  //   const data = {};
-  //   beforeAll(done => {
-  //     Request.get('http://localhost:3000/test', (error, response, body) => {
-  //       data.status = response.statusCode;
-  //       data.body = JSON.parse(body);
-  //       done();
-  //     });
-  //   });
-  //   it('Status 200', () => {
-  //     expect(data.status).toBe(500);
-  //   });
-  //   it('Body', () => {
-  //     expect(data.body.message).toBe('This is an error response');
-  //   });
-  // });
 });
