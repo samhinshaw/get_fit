f59ce54f9317e8c919f6a6f35d7456851d0c78e7
Sam Hinshaw <samuel.hinshaw@gmail.com>
2019-01-15 07:23:21 -0800

    Use jasmine spec reporter

--- tests/config/run.js
+++ tests/config/run.js
@@ -1,5 +1,19 @@
 import Jasmine from 'jasmine';
+import SpecReporter from 'jasmine-spec-reporter';
 
 const jasmine = new Jasmine();
+
 jasmine.loadConfigFile('tests/config/jasmine.json');
+
+// remove default reporter logs
+jasmine.getEnv().clearReporters();
+// add jasmine-spec-reporter
+jasmine.getEnv().addReporter(
+  new SpecReporter({
+    spec: {
+      displayPending: true
+    }
+  })
+);
+
 jasmine.execute();
