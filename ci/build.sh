66baeaca7f675c22e2ff837b1b8c38a7854c6dbb
Sam Hinshaw <samuel.hinshaw@gmail.com>
2019-01-16 08:34:40 -0800

    Debug statements in build step

--- ci/build.sh
+++ ci/build.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
+echo "Travis Tag: $TRAVIS_TAG"
+echo "Travis Branch: $TRAVIS_BRANCH"
+
 # If this build was tagged, generate a docker tag with that tag. 
 # Otherwise, generate a docker tag from the release branch name.
 if [[ -n "$TRAVIS_TAG" ]]; then
-  DOCKER_TAG="$TRAVIS_TAG-$TRAVIS_BUILD_NUMBER"
+  export DOCKER_TAG="$TRAVIS_TAG-$TRAVIS_BUILD_NUMBER"
+  echo "Tagging Docker image with: $DOCKER_TAG"
 elif [[ $TRAVIS_BRANCH == release* ]]; then
   # Cut any preceding branch at "/"
   CUT_BRANCH=`echo "$TRAVIS_BRANCH" | cut -d "/" -f 2`
-  DOCKER_TAG="$CUT_BRANCH-$TRAVIS_BUILD_NUMBER"
+  export DOCKER_TAG="$CUT_BRANCH-$TRAVIS_BUILD_NUMBER"
+  echo "Tagging Docker image with: $DOCKER_TAG"
+else
+  echo "Not tagging Docker image."
 fi
 
 yarn build:test
