dbfe1271eddc1d5ab34175df76ff9d6817f95aac
Sam Hinshaw <samuel.hinshaw@gmail.com>
2019-01-15 20:05:19 -0800

    Use proper bash syntax for else if (elif)

--- ci/build.sh
+++ ci/build.sh
@@ -4,7 +4,7 @@
 # Otherwise, generate a docker tag from the release branch name.
 if [[ -n "$TRAVIS_TAG" ]]; then
   DOCKER_TAG="$TRAVIS_TAG-$TRAVIS_BUILD_NUMBER"
-else if [[ $TRAVIS_BRANCH == release* ]]; then
+elif [[ $TRAVIS_BRANCH == release* ]]; then
   # Cut any preceding branch at "/"
   CUT_BRANCH=`echo "$TRAVIS_BRANCH" | cut -d "/" -f 2`
   DOCKER_TAG="$CUT_BRANCH-$TRAVIS_BUILD_NUMBER"
