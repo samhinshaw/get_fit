3da3d063e4f12f7f5a41bc9f76b3879240e5d00d
Sam Hinshaw <samuel.hinshaw@gmail.com>
2019-01-17 20:13:06 -0800

    Don't specify custom 10s timeout

--- tests/app.spec.js
+++ tests/app.spec.js
@@ -14,26 +14,22 @@ describe('Server', () => {
     beforeAll(() => {
       backendAddress = `http://${backendHost}:8005/`;
     });
-    it(
-      'Status 200',
-      done => {
-        // eslint-disable-next-line no-console
-        console.log(`Connecting to ${backendAddress}`);
-        axios
-          .get(backendAddress)
-          .then(resp => {
-            // eslint-disable-next-line no-console
-            expect(resp.status).toBe(200);
-            done();
-          })
-          .catch(err => {
-            // eslint-disable-next-line no-console
-            console.error(err);
-            fail(`The request to ${backendAddress} failed.`);
-            done();
-          });
-      },
-      10 * 1000
-    );
+    it('Status 200', done => {
+      // eslint-disable-next-line no-console
+      console.log(`Connecting to ${backendAddress}`);
+      axios
+        .get(backendAddress)
+        .then(resp => {
+          // eslint-disable-next-line no-console
+          expect(resp.status).toBe(200);
+          done();
+        })
+        .catch(err => {
+          // eslint-disable-next-line no-console
+          console.error(err);
+          fail(`The request to ${backendAddress} failed.`);
+          done();
+        });
+    });
   });
 });
