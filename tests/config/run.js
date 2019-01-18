606779119da8b82352849c316561f47f76b062b0
Sam Hinshaw <samuel.hinshaw@gmail.com>
2019-01-17 20:13:15 -0800

    Be more explicit about waiting

--- tests/config/run.js
+++ tests/config/run.js
@@ -33,12 +33,19 @@ tcpPortUsed
     // on our target host, either "localhost" or "node"
     backendHost,
     // wait 100ms between pings
-    100,
+    5 * 1000,
     // wait 20sec in total for the port to be used
     20 * 1000
   )
   // Then start tests
-  .then(() => jasmine.execute())
+  .then(() => {
+    // eslint-disable-next-line no-console
+    console.log('Waiting for backend to be available...');
+    // Then wait another 10 seconds for good measure
+    setTimeout(() => {
+      jasmine.execute();
+    }, 10 * 1000);
+  })
   // Otherwise log errors
   // eslint-disable-next-line no-console
   .catch(err => console.error('Error on check:', err.message));
