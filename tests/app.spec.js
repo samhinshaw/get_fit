04456a76be47375e8b7cd1c22da60dd217a9bc81
Sam Hinshaw <samuel.hinshaw@gmail.com>
2019-04-13 15:28:31 -0700

    Test our user page as well

--- tests/app.spec.js
+++ tests/app.spec.js
@@ -1,34 +1,55 @@
 /* eslint-env jasmine */
+/* eslint-disable jasmine */
 import axios from 'axios';
 
 describe('The server', () => {
-  // Use localhost if we're in development (running our tests outside of a
-  // container), otherwise use node (inside a container)
-  let backendHost;
+  // If we're in test, use `node` (will allow networking between Docker
+  // containers). Otherwise, if we're in development or there is no NODE_ENV
+  // (running outside a container, perhaps), use `localhost`
+  let address;
   beforeAll(() => {
     // Set default timeout
-    backendHost = process.env.NODE_ENV === 'development' ? 'localhost' : 'node';
+    const host = process.env.NODE_ENV === 'test' ? 'node' : 'localhost';
+    address = `http://${host}:8005`;
   });
 
   describe('serves /', () => {
-    let backendAddress;
+    let route;
     beforeAll(() => {
-      backendAddress = `http://${backendHost}:8005/`;
+      route = `${address}/`;
     });
+
     it('with status 200', done => {
-      // eslint-disable-next-line no-console
-      console.log(`Connecting to ${backendAddress}`);
       axios
-        .get(backendAddress)
+        .get(route)
         .then(resp => {
-          // eslint-disable-next-line no-console
           expect(resp.status).toBe(200);
           done();
         })
         .catch(err => {
-          // eslint-disable-next-line no-console
           console.error(err);
-          fail(`The request to ${backendAddress} failed.`);
+          fail(`The request to ${route} failed.`);
+          done();
+        });
+    });
+  });
+
+  describe('serves /user', () => {
+    let route;
+    beforeAll(() => {
+      route = `${address}/user`;
+    });
+
+    it('with status 200', done => {
+      axios
+        .get(route)
+        .then(resp => {
+          expect(resp.status).toBe(200);
+          done();
+        })
+        .catch(err => {
+          console.error(err);
+          fail(`The request to ${route} failed.`);
           done();
         });
     });
