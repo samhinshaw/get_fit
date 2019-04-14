b9df8853312e4eb4652ebf7a05f5063fc6b0c2d1
Sam Hinshaw <samuel.hinshaw@gmail.com>
2019-04-13 17:50:37 -0700

    Almost have authentication tests working

--- tests/app.spec.js
+++ tests/app.spec.js
@@ -6,7 +6,7 @@ const FormData = require('form-data');
 
 const waitTimes = {
   // wait a long time for the login, since we increased our bcrypt cost factor significantly
-  LOGIN: 30 * 1000,
+  LOGIN: 10 * 1000,
 };
 
 describe('Get Fit', () => {
@@ -20,59 +20,16 @@ describe('Get Fit', () => {
     address = `http://${host}:8005`;
   });
 
-  describe('the home page', () => {
-    let route;
-    beforeAll(() => {
-      route = `${address}/`;
-    });
+  describe('unauthenticated requests', () => {
+    describe('the home page', () => {
+      let route;
+      beforeAll(() => {
+        route = `${address}/`;
+      });
 
-    it('should be served with status 200', done => {
-      axios
-        .get(route, { maxRedirects: 0 })
-        .then(resp => {
-          expect(resp.status).toBe(200);
-          done();
-        })
-        .catch(err => {
-          console.error(err);
-          fail(`The request to ${route} failed.`);
-          done();
-        });
-    });
-  });
-
-  describe('the login page', () => {
-    let route;
-    beforeAll(() => {
-      route = `${address}/login`;
-    });
-
-    it('should be served with status 200', done => {
-      axios
-        .get(route, { maxRedirects: 0 })
-        .then(resp => {
-          expect(resp.status).toBe(200);
-          done();
-        })
-        .catch(err => {
-          console.error(err);
-          fail(`The request to ${route} failed.`);
-          done();
-        });
-    });
-
-    let form;
-    beforeEach(() => {
-      form = new FormData();
-      form.append('username', 'john');
-      form.append('password', 'testpassword');
-    });
-
-    it(
-      'should log in the user when given correct credentials',
-      done => {
+      it('should be served with status 200', done => {
         axios
-          .post(route, form, { headers: form.getHeaders() })
+          .get(route, { maxRedirects: 0 })
           .then(resp => {
             expect(resp.status).toBe(200);
             done();
@@ -82,49 +39,151 @@ describe('Get Fit', () => {
             fail(`The request to ${route} failed.`);
             done();
           });
-      },
-      waitTimes.LOGIN
-    );
+      });
+    });
 
-    it(
-      'should redirect a user to the overview page when given correct credentials',
-      done => {
+    describe('the login page', () => {
+      let route;
+      beforeAll(() => {
+        route = `${address}/login`;
+      });
+
+      it('should be served with status 200', done => {
         axios
-          .post(route, form, { headers: form.getHeaders(), maxRedirects: 0 })
+          .get(route, { maxRedirects: 0 })
           .then(resp => {
-            console.log('it happened');
-            fail(`The request did not redirect, status code ${resp.status}.`);
+            expect(resp.status).toBe(200);
             done();
           })
           .catch(err => {
-            console.log('it caught');
-            expect(err.response.status).toBe(302);
-            expect(err.response.headers.location).toBe('/overview');
+            console.error(err);
+            fail(`The request to ${route} failed.`);
             done();
           });
-      },
-      waitTimes.LOGIN
-    );
-  });
+      });
 
-  describe('the user page', () => {
-    let route;
-    beforeAll(() => {
-      route = `${address}/user`;
+      let form;
+      beforeEach(() => {
+        form = new FormData();
+        form.append('username', 'john');
+        form.append('password', 'testpassword');
+      });
+
+      it(
+        'should log in the user when given correct credentials and set a session cookie',
+        done => {
+          axios
+            .post(route, form, { headers: form.getHeaders() })
+            .then(resp => {
+              expect(resp.status).toBe(200);
+              expect(resp.headers['set-cookie']).toBeTruthy();
+              done();
+            })
+            .catch(err => {
+              console.error(err);
+              fail(`The request to ${route} failed.`);
+              done();
+            });
+        },
+        waitTimes.LOGIN
+      );
+
+      it(
+        'should redirect a user to the overview page when given correct credentials',
+        done => {
+          axios
+            .post(route, form, {
+              headers: form.getHeaders(),
+              maxRedirects: 0,
+              validateStatus: null,
+            })
+            .then(res => {
+              expect(res.status).toBe(302);
+              expect(res.headers.location).toBe('/overview');
+              done();
+            })
+            .catch(err => {
+              fail(err);
+              done();
+            });
+        },
+        waitTimes.LOGIN
+      );
     });
 
-    it('should redirect us to the login page', done => {
+    describe('the user page', () => {
+      let route;
+      beforeAll(() => {
+        route = `${address}/user`;
+      });
+
+      it('should redirect us to the login page', done => {
+        axios
+          .get(route, { maxRedirects: 0, validateStatus: null })
+          .then(res => {
+            expect(res.status).toBe(302);
+            expect(res.headers.location).toBe('/login');
+            done();
+          })
+          .catch(err => {
+            fail(err);
+            done();
+          });
+      });
+    });
+  });
+
+  describe('authenticated requests', () => {
+    let authCookie = '';
+    beforeAll(done => {
+      const route = `${address}/login`;
+      const loginForm = new FormData();
+      loginForm.append('username', 'john');
+      loginForm.append('password', 'testpassword');
+
       axios
-        .get(route, { maxRedirects: 0 })
-        .then(resp => {
-          fail(`The request did not redirect, status code ${resp.status}.`);
-          done();
+        .post(route, loginForm, {
+          headers: loginForm.getHeaders(),
+          withCredentials: true,
+          maxRedirects: 0,
+          validateStatus: null,
         })
-        .catch(err => {
-          expect(err.response.status).toBe(302);
-          expect(err.response.headers.location).toBe('/login');
+        .then(res => {
+          if (res.headers.location === '/overview') {
+            const cookies = res.headers['set-cookie'] || [];
+            authCookie = cookies.find(cookie => cookie.includes('connect.sid'));
+          } else {
+            throw new Error('authentication unsuccessful');
+          }
           done();
         });
+    }, waitTimes.LOGIN);
+
+    describe('the user page', () => {
+      let route;
+      beforeAll(() => {
+        route = `${address}/user`;
+      });
+
+      xit('should render for authenticated requests', done => {
+        axios
+          .get(route, {
+            headers: {
+              Cookie: authCookie,
+            },
+            maxRedirects: 0,
+            validateStatus: null,
+          })
+          .then(res => {
+            expect(res.status).toBe(302);
+            expect(res.headers.location).toBe('/user');
+            done();
+          })
+          .catch(err => {
+            fail(err);
+            done();
+          });
+      });
     });
   });
 });
